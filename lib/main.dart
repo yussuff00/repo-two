e6e1f60d0bf4e836d7e58ac0b50d5360ecb2049f
yussuff00 <yussufalaa009@gmail.com>
2026-03-15 20:25:31 +0200

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -17,7 +17,6 @@ class MyApp extends StatelessWidget {
         // TRY THIS: Try running your application with "flutter run". You'll see
         // the application has a purple toolbar. Then, without quitting the app,
         // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
         // reload" button in a Flutter-supported IDE, or press "r" if you used
         // the command line to start the app).
         //
